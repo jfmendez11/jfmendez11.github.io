e8a0a03afd33d600bd58c57493b9e63a45ecb9e0
jfmendez11 <jf.mendez11@uniandes.edu.co>
2019-07-24 17:23:52 -0500

    Updates

---
+++ precache-manifest.73c07a6848e7a99a89fa0515276a85b0.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1ab63b9e6cb05dad0f76a0a3f4b3420",
+    "url": "/index.html"
+  },
+  {
+    "revision": "635deed6bcef5c71126a",
+    "url": "/static/css/main.93aaa6a9.chunk.css"
+  },
+  {
+    "revision": "90db47ffe457261cba53",
+    "url": "/static/js/2.ad501400.chunk.js"
+  },
+  {
+    "revision": "635deed6bcef5c71126a",
+    "url": "/static/js/main.733acb27.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e404f13f69ff36807524eef90b6356f8",
+    "url": "/static/media/04-A-Crime-On-Your-Hands.e404f13f.m4a"
+  },
+  {
+    "revision": "d21ab8578ebfe0294fb71cb63b3e6476",
+    "url": "/static/media/Captura de Pantalla 2019-07-15 a la(s) 11.03.21 a. m..d21ab857.png"
+  },
+  {
+    "revision": "f2ccb22946b4d075a6100bf206de4a0e",
+    "url": "/static/media/cv_mendez_juanfelipe.f2ccb229.pdf"
+  },
+  {
+    "revision": "933500e31156ec6711a862d64f448467",
+    "url": "/static/media/megadethmast-1.933500e3.jpg"
+  },
+  {
+    "revision": "3f6ada77049cb3faee3779473dfb0468",
+    "url": "/static/media/metallicaheavymontreal2017.3f6ada77.jpg"
+  },
+  {
+    "revision": "71c4d3888b65f61468d012e1e4b299de",
+    "url": "/static/media/millos.71c4d388.png"
+  },
+  {
+    "revision": "3246538a2bd5cdfe86538d1102c9275f",
+    "url": "/static/media/seleccion.3246538a.png"
+  },
+  {
+    "revision": "9c9f8b858c3d36fba2dc4bcbfc21e2db",
+    "url": "/static/media/system-of-a-down1.9c9f8b85.jpg"
+  },
+  {
+    "revision": "aaa70615902fea5ce3045ed5151f2764",
+    "url": "/static/media/united.aaa70615.png"
+  }
+]);
